a10bef265a8415d53e50b35bac9b4c61bd34fbdd
Fabrice L <fabrice@MacBook-Pro-de-Fabrice.home>
2021-11-09 21:53:55 +0100

    added Navigation

--- components/Search.js
+++ components/Search.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import Button from './common/Button.js';
-import style from '../styles.js'
+import style from '../styles.js';
 
-export default function Home({ navigation }) {
-  const [text, setText] = useState('Meudon');
+const Search = ({ navigation }) => {
+  const [text, setText] = useState('');
 
   const search = () => {
-    navigation.navigate('About');
-  }
+    navigation.navigate('Details');
+  };
 
   return (
     <View style={styles.container}>
@@ -17,26 +17,32 @@ export default function Home({ navigation }) {
         style={styles.textInput}
         onChangeText={(value) => setText(value)}
         value={text}
+        onSubmitEditing={() => search()}
+        placeholder='Entrez une ville'
       />
 
       <Button
         onPress={() => search()}
-        title="Rechercher"
+        title='Rechercher'
         style={style.button}
-        /> 
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   textInput: {
     height: 40,
+    borderRadius: 5,
     borderColor: 'gray',
     borderWidth: 1,
-    marginHorizontal: 10
-  }
-})
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+  },
+});
+
+export default Search;
