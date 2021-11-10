e0b2a22e702038e452135137942c4933d62facc7
fabriceL <fabrice.latri@formasup-paris.com>
2021-11-10 16:05:06 +0100

    Added IOS animation navigation effect

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import About from '../About';
-import Search from '../Search';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { StatusBar } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import DetailsScreen from '../DetailsScreen';
+import React from "react";
+import About from "../About";
+import Search from "../Search";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DetailsScreen from "../DetailsScreen";
 
-const SearchStackNavigator = createNativeStackNavigator();
-const AboutStackNavigator = createNativeStackNavigator();
+const SearchStackNavigator = createStackNavigator();
+const AboutStackNavigator = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const SearchStackScreen = () => {
@@ -18,16 +21,20 @@ const SearchStackScreen = () => {
       <SearchStackNavigator.Group
         screenOptions={{
           headerStyle: {
-            backgroundColor: 'tomato',
+            backgroundColor: "tomato",
           },
-          headerTintColor: '#FFF',
-        }}>
+          headerTintColor: "#FFF",
+          ...TransitionPresets.SlideFromRightIOS,
+        }}
+      >
         <SearchStackNavigator.Screen
-          name='SearchScreen'
-          component={Search}></SearchStackNavigator.Screen>
+          name="SearchScreen"
+          component={Search}
+        ></SearchStackNavigator.Screen>
         <SearchStackNavigator.Screen
-          name='Details'
-          component={DetailsScreen}></SearchStackNavigator.Screen>
+          name="Details"
+          component={DetailsScreen}
+        ></SearchStackNavigator.Screen>
       </SearchStackNavigator.Group>
     </SearchStackNavigator.Navigator>
   );
@@ -37,8 +44,9 @@ const AboutStackScreen = () => {
   return (
     <AboutStackNavigator.Navigator>
       <AboutStackNavigator.Screen
-        name='AboutScreen'
-        component={About}></AboutStackNavigator.Screen>
+        name="AboutScreen"
+        component={About}
+      ></AboutStackNavigator.Screen>
     </AboutStackNavigator.Navigator>
   );
 };
@@ -50,24 +58,25 @@ const BottomTabsNavigation = () => {
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
-          if (route.name === 'Search') {
-            iconName = focused ? 'ios-search' : 'ios-search-outline';
-          } else if (route.name === 'About') {
+          if (route.name === "Search") {
+            iconName = focused ? "ios-search" : "ios-search-outline";
+          } else if (route.name === "About") {
             iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
+              ? "ios-information-circle"
+              : "ios-information-circle-outline";
           }
 
           // You can return any component that you like here!
           return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
         tabBarHideOnKeyboard: true,
         headerShown: false,
-      })}>
-      <Tab.Screen name='Search' component={SearchStackScreen} />
-      <Tab.Screen name='About' component={AboutStackScreen} />
+      })}
+    >
+      <Tab.Screen name="Search" component={SearchStackScreen} />
+      <Tab.Screen name="About" component={AboutStackScreen} />
     </Tab.Navigator>
   );
 };
